836f876a569683ce921768693098f17b21365c53
Jack Wotherspoon <jackwoth@google.com>
2025-05-23 16:48:29 -0400

    add video generation

--- content-generation-agent/agent.py
+++ content-generation-agent/agent.py
@@ -2,13 +2,13 @@
 
 from google.adk import Agent
 from google.adk.tools import load_artifacts
-from .tools import generate_image, upload_image_to_gcs
+from .tools import generate_image, generate_video, upload_image_to_gcs
 
 MODEL = "gemini-2.5-pro-preview-05-06" 
 
 root_agent = Agent(
     model=MODEL,
     name="content_agent",
-    instruction="You are an agent whose job is to generate or edit an image based on the prompt provided",
-    tools=[generate_image, upload_image_to_gcs, load_artifacts],
+    instruction="You are an agent whose job is to generate or edit an image or video based on the prompt provided",
+    tools=[generate_image, generate_video, upload_image_to_gcs, load_artifacts],
 )
--- content-generation-agent/tools.py
+++ content-generation-agent/tools.py
@@ -1,4 +1,6 @@
 
+import asyncio
+import logging
 import os
 import uuid
 
@@ -8,8 +10,11 @@ from google.cloud import storage
 from google.genai import Client, types
 
 MODEL_IMAGE = "imagen-4.0-generate-preview-05-20"
+MODEL_VIDEO = "veo-2.0-generate-preview"
 
 load_dotenv()
+
+logger = logging.getLogger(__name__)
 
 # Only Vertex AI supports Imagen 4 for now.
 client = Client(
@@ -51,6 +56,76 @@ async def generate_image(img_prompt: str, tool_context: ToolContext):
         "filename": filename,
     }
 
+async def generate_video(
+    video_prompt: str,
+    tool_context: ToolContext,
+    number_of_videos: int = 1,
+    aspect_ratio: str = "16:9",
+    negative_prompt: str = "",
+    existing_image_filename: str = "",
+):
+    """Generates a video based on the prompt.
+
+    Args:
+        video_prompt (str): The prompt to generate the video from.
+        tool_context (ToolContext): The tool context.
+        number_of_videos (int, optional): The number of videos to generate. Defaults to 1.
+        aspect_ratio (str, optional): The aspect ratio of the video. Defaults to "16:9".
+        negative_prompt (str, optional): The negative prompt to use. Defaults to "".
+
+    Returns:
+        dict: status dict
+
+    Supported aspect ratios are:
+        16:9 (landscape) and 9:16 (portrait) are supported.
+    """
+    gen_config = types.GenerateVideosConfig(
+        aspect_ratio=aspect_ratio,
+        number_of_videos=number_of_videos,
+        output_gcs_uri=os.environ["BUCKET"],
+        negative_prompt=negative_prompt,
+    )
+    # If an existing image is provided, use it to generate the video.
+    if existing_image_filename:
+        gcs_location = f"{os.environ['BUCKET']}/{existing_image_filename}"
+        existing_image = types.Image(gcs_uri=gcs_location, mime_type="image/png")
+        operation = client.models.generate_videos(
+            model=MODEL_VIDEO,
+            prompt=video_prompt,
+            image=existing_image,
+            config=gen_config,
+        )
+    else:
+        operation = client.models.generate_videos(
+            model=MODEL_VIDEO, prompt=video_prompt, config=gen_config
+        )
+    while not operation.done:
+        logger.info("--- ⏳ Waiting for video generation to be done... ---")
+        await asyncio.sleep(10)
+        operation = client.operations.get(operation)
+        print(operation)
+
+    if operation.response:
+        # Download and save the generated videos to artifacts.
+        for generated_video in operation.result.generated_videos:
+            video_uri = generated_video.video.uri
+            filename = f"{uuid.uuid4()}.mp4"
+            bucket = os.getenv("BUCKET")
+            video_bytes = download_blob_from_gcs(
+                bucket.replace("gs://", ""),
+                video_uri.replace(bucket, "")[1:],  # get rid of trailing slash
+            )
+            print(f" --- 🗄️ The location for the saved video is here: {filename} --- ")
+            await tool_context.save_artifact(
+                filename,
+                types.Part.from_bytes(data=video_bytes, mime_type="video/mp4"),
+            )
+        return {
+            "status": "success",
+            "detail": "Video generated successfully and stored in artifacts.",
+            "filename": filename,
+            }
+
 async def upload_image_to_gcs(
     filename: str, tool_context: ToolContext, gcs_bucket: str = os.environ["BUCKET"]
 ):
@@ -70,9 +145,16 @@ async def upload_image_to_gcs(
     # Get the image from ADK artifacts
     image_artifact = await tool_context.load_artifact(filename)
     if image_artifact and image_artifact.inline_data:
-        print(f"Successfully loaded latest ADK artifact '{filename}'.")
+        logger.info(f" --- ✅ Successfully loaded latest ADK artifact '{filename}' --- ")
         image_bytes = image_artifact.inline_data.data
         blob.upload_from_string(image_bytes, content_type="image/png")
     else:
-        print(f"Failed to load ADK artifact '{filename}'.")
+        logger.info(f" --- ❌ Failed to load ADK artifact '{filename}'. --- ")
     return f"gs://{gcs_bucket}/{filename}"
+
+def download_blob_from_gcs(bucket_name: str, source_blob_name: str):
+    """Downloads a blob from the Google Cloud Storage bucket."""
+    storage_client = storage.Client()
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(source_blob_name)
+    return blob.download_as_bytes()
